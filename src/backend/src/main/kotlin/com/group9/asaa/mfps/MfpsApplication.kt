2665bf474b345aefaba2fe9e049b75f2cacd1403
joao-cardoso-challenge <joao.cardoso@challenge.pt>
2025-11-12 14:23:48 +0100

    feat: docker containers configured

--- src/backend/src/main/kotlin/com/group9/asaa/mfps/MfpsApplication.kt
+++ src/backend/src/main/kotlin/com/group9/asaa/mfps/MfpsApplication.kt
@@ -2,10 +2,23 @@ package com.group9.asaa.mfps
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class MfpsApplication
 
 fun main(args: Array<String>) {
 	runApplication<MfpsApplication>(*args)
+}
+
+
+@RestController
+@RequestMapping("/mfps")
+class MfpsController {
+	@RequestMapping("/system-status")
+	fun status(): ResponseEntity<String> {
+		return ResponseEntity.ok().body("Mfps System is running")
+	}
 }
