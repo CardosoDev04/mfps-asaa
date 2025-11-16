cec9a1e72e5d95c2036c90694e5f5b527d9d6e19
joao-cardoso-challenge <joao.cardoso@challenge.pt>
2025-11-16 15:35:57 +0100

    feat: missing files and docker compose

--- src/dashboard/src/services/assembly.ts
+++ src/dashboard/src/services/assembly.ts
@@ -1,14 +1,23 @@
 import type { AssemblyTransportOrder } from "../@types/AssemblyTransportOrder";
 
-export async function createOneAssemblyOrder(demo: boolean): Promise<AssemblyTransportOrder> {
-  const res = await fetch(`http://localhost:8080/assembly/transport-order?demo=${demo}`, {
-    method: "POST",
-  });
+export async function createOneAssemblyOrder(
+  demo: boolean
+): Promise<AssemblyTransportOrder> {
+  const res = await fetch(
+    `http://localhost:8080/assembly/transport-order?demo=${demo}`,
+    {
+      method: "POST",
+    }
+  );
   const order: AssemblyTransportOrder = await res.json();
-  return order
+  return order;
 }
 
-export async function createBulkAssemblyOrders(demo: boolean, count: number, testRunId: string): Promise<AssemblyTransportOrder[]> {
+export async function createBulkAssemblyOrders(
+  demo: boolean,
+  count: number,
+  testRunId: string
+): Promise<AssemblyTransportOrder[]> {
   const res = await fetch(
     `http://localhost:8080/assembly/transport-order/bulk?n=${count}&demo=${demo}&testRunId=${testRunId}`,
     { method: "POST" }
